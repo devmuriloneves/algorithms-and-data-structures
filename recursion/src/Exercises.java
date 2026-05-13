4b25631253bee3d33c24a84915cadc6818130363
devmuriloneves <muriloneves355@gmail.com>
2026-05-12 23:27:34 -0300

    feat: add recursive exercises for array traversal and min value search

--- recursion/src/Exercises.java
+++ recursion/src/Exercises.java
@@ -1,3 +1,47 @@
 public class Exercises {
+    // Create a recursive method that prints an array received as parameter in reverse order
+    public static <T> void printArrayReverse(T[] array) {
+        printArrayReverse(array, array.length -1);
+    }
 
+    private static <T> void printArrayReverse(T[] array, int index) {
+        if (index == 0) {
+            System.out.println(array[0]);
+            return;
+        }
+        System.out.println(array[index]);
+        printArrayReverse(array, index - 1);
+    }
+
+    /* Create a recursive method that receives a 2D double array
+       and returns the sum of its elements */
+    public static double doubleArraySum(double[][] array) {
+        return doubleArraySum(array, 0, 0);
+    }
+
+    private static double doubleArraySum(double[][] array, int line, int column) {
+        if (line == array.length) {
+            return 0;
+        } else if (column == array[line].length - 1) {
+            return array[line][column] + doubleArraySum(array, line + 1, 0);
+        } else {
+            return array[line][column] + doubleArraySum(array, line, column + 1);
+        }
+    }
+
+    /* Create a method called findMinValueIndex. This method must find the position
+    of the smallest value in an integer array received as parameter. */
+    public static int findMinValueIndex(int[] array) {
+        return findMinValueIndex(array, 0);
+    }
+
+    private static int findMinValueIndex(int[] array, int index) {
+        if (index == array.length - 1)
+            return index;
+        int minIndex = findMinValueIndex(array, index + 1);
+        if (array[index] <= array[minIndex])
+            return index;
+        else
+            return minIndex;
+    }
 }
---
+++ recursion/src/RecursionExercisesRunner.java
@@ -0,0 +1,10 @@
+// Entry point used to test the methods from the Exercises class
+public class RecursionExercisesRunner {
+    public static void main(String[] args) throws Exception {
+        Exercises.printArrayReverse(new Integer[] {1, 2, 8, 6, 7});
+
+        System.out.println(Exercises.doubleArraySum(new double[][] {{1.0, 2.0}, {3.0, 4.0}}));
+
+        System.out.println(Exercises.findMinValueIndex(new int[] {3, 1, 4, 1, 5}));
+    }
+}
