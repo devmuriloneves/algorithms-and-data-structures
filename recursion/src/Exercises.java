9b395bda7d2c80a13e4b3a7324aad779e63eabf8
devmuriloneves <muriloneves355@gmail.com>
2026-06-04 14:10:21 -0300

    feat: add recursive primary diagonal and binary-to-decimal exercises

--- recursion/src/Exercises.java
+++ recursion/src/Exercises.java
@@ -44,4 +44,32 @@ public class Exercises {
         else
             return minIndex;
     }
+
+    public static <T> void printPrimaryDiagonal(T[][] array) {
+        printPrimaryDiagonal(array, 0, 0);
+    }
+
+    private static <T> void printPrimaryDiagonal(T[][] array, int line, int column) {
+        if (line == array.length)
+            return;
+        else if (column < array[line].length)
+            System.out.println(array[line][column]);
+        printPrimaryDiagonal(array, line + 1, column + 1);
+    }
+
+    public static int binaryToDecimal(String binary) {
+        if (!binary.matches("[01]{8}"))
+            return -1;
+        return binaryToDecimal(binary, 0);
+    }
+
+    private static int binaryToDecimal(String binary, int index) {
+        if (index == binary.length())
+            return 0;
+        int number = Character.getNumericValue(binary.charAt(index));
+        if (number == 1)
+            return (int) Math.pow(2, binary.length() - 1 - index) + binaryToDecimal(binary, index + 1);
+        else
+            return binaryToDecimal(binary, index + 1);
+    }
 }
--- recursion/src/RecursionExercisesRunner.java
+++ recursion/src/RecursionExercisesRunner.java
@@ -6,5 +6,7 @@ public class RecursionExercisesRunner {
         System.out.println(Exercises.doubleArraySum(new double[][] {{1.0, 2.0}, {3.0, 4.0}}));
 
         System.out.println(Exercises.findMinValueIndex(new int[] {3, 1, 4, 1, 5}));
+
+        System.out.println(Exercises.binaryToDecimal("11010001"));
     }
 }
